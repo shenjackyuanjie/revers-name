2f81c140eac0543bb8afa7002619b700413559a5
shenjack <3695888@qq.com>
2024-03-17 22:12:39 +0800

    滚去写作业

--- node_run/static/md5.js
+++ node_run/static/md5.js
@@ -3373,19 +3373,19 @@
                     }
                 }
             },
-            wrap_js_function_for_async(a) {
-                var s = function (b, c) {
-                    return function (d, e) {
+            wrap_js_function_for_async(func) {
+                var protected = function (fn, ERROR) {
+                    return function (error_code, result) {
                         while (true) try {
-                            b(d, e)
+                            fn(error_code, result)
                             break
-                        } catch (r) {
-                            e = r
-                            d = c
+                        } catch (error) {
+                            result = error
+                            error_code = ERROR
                         }
                     }
-                }(a, 1)
-                return $.P.ct(new P.lr(s))
+                }(func, 1)
+                return $.P.ct(new P.lr(protected))
             },
             async_error(a, b) {
                 var s = H.ls(a, "error", t.K)
@@ -4395,17 +4395,17 @@
         },
         Q = {
             main_func() {
-                var switch_branch = 0,
-                    r = P.make_async_await_completer(t.z),
+                var async_goto = 0,
+                    async_completer = P.make_async_await_completer(t.z),
                     q, switch_to = 2,
-                    o, n = [],
+                    async_result_1, n = [],
                     m, l, k, j, input_name, h, g, f, e, d, c, b, a, a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, b0
-                var $async$iE = P.wrap_js_function_for_async(function (b1, b2) {
-                    if (b1 === 1) {
-                        o = b2
-                        switch_branch = switch_to
+                var $async$iE = P.wrap_js_function_for_async(function (error_code, async_result) {
+                    if (error_code === 1) {
+                        async_result_1 = async_result
+                        async_goto = switch_to
                     }
-                    while (true) switch (switch_branch) {
+                    while (true) switch (async_goto) {
                         case 0:
                             // await HtmlRenderer.init();
 
@@ -4423,7 +4423,7 @@
                             */
                             // 抄tinman的https://tinman00.github.io/100/namerena/md5.js
                             $.ox = `@@ABMECIGUB@OK@CHBAA@FCMFNBEABCICEG@DJDGBGHMDALBAHHEDIEFB@AACFLCFDC@ABIC@DBLIABFPCFDCTD@B@@ID@@EHDI@GDGFDAH@ABBAGBBF@PKALADHG@AAOC@EJ@@FCHC@MLKBAHBGDAKDGA\\C@A@FHE@TTHWCGHJBBAFBSGCAAF@I@D@A@AC@AGIFXHBAEU@@@AMTGFF@AAIBJGAKAAAE@BJIMH@CAAHAABC@DD@L@AABEDFECBCT@BGED@GDF@CFDDGH@ACDBDH@DAFOBBIKD@ICJGCAH@GBADBDGDH@@DIY@BFDEOEAS@G@DIN@GABNHECOCBEAHPBC@AIBDAFBDWB@GCAB@EACD@DE@@FFDB@JBFAENJ@L@JMM@R@JD@@DBCFCDAB@@@EBABCEA@PAEBKB@@@DXBCACFABCDFBIAFDBAEAFCY@LB@EHH@B@BIIDFGDOCH@EB@LFCCCDCE@BF@DG@LNDQKKCC@FACDFCGBCSC@CLI@CHLBHELHA@BCA@AABSBBBNBFI@MBCMB@UB@PNA@DEJ_GOD@DKDBIMC@BMBBADFVEB@DCBA@AG@HE@FDCE@AC@EBBL]@AF@@A@FBHBECJAFDAMBA@EBEICGFDCEBADMC@LC@FH@D@@EEBAFPAHIAAACBBD@BBBCCDCB@GDHCMHAGAGA@HVBFCRCDHCJBERJBBEBEIGOAD@WBDCDKAACK@BAMAOADFE@@EUFAF@NKICDFBFWLAIICBABBKWBGLLZAGHBA@AB@A@DEBG@HER@CDAO@@FCDA@EOFB@DBOJRAAAABCAFFDBAAKDB@ACD@FC@I@@EB@HHCBGCCLCB@BDBA@BFBBHNIA@DDQA@@@EAO@EHIGBOAFSAZCGJRCN@CMJL@CACE@HCIDTBGSFAA@AA@DFBA@J@DJDGEBEFBBHIJ@GCFGDLBA@@DBBD@FA@TBCAIHAJ@CCLHDA@DHFF@HJCF@DEAJE@JGI@ABFJEDKD@DGEICHECPMDBIELA@D[F@HDGDABBND@CAKHBCCDOAJACEE@CIDFVALABQAC@P@HBBBKBA@AM@@RCAC@PKD@HC@@IABD@PCFFA@NQD@DBJFJCAAA@HACFFBAAJ@AM@GDBECBADD@D@@CNK@@FPEJGDATACFJQANKADFA@LKVGEAJQAAB@@KDA@C@DCAEBAADBNHCCE@@@AACGED@JBBFBHANAFMAASHDC@DBIJJHDECAD@FDAFECDGC@GHQB@@BBCEFEXABBA@@EIIJF@@DEC@AAQCA@A@CIBGNACK@CHLEFBCBD@BOIB@BJDEGFD@AL@INECCAUAXHBBEDWBBA@BBLDADD@BHB@D@HHFB@GGAKABAAEGCKFJHJC@@@HECDDVBL@BCLB@DEEE@BDFN[@DPA@DF@AAFF@B@EGEDG@ABBBFCAAA@FPAJBBACBBFGA@@@D@LOOBBQDGDOJRACGABCUCAHGBF@BBKDAA@FCCO[AAIALA@SKHKB@ABRLDDBC@CAEAFBGFCLA@DBFFB@A@EG]DEGEBQWKMB@NBBMEADKI^@@@KHED@P@CC@FACZNADAM@IADC@V@@@A@IO@CKEEKCBLIBCBG@AEBCANA@BJAFBAVCLDGHCKPDEDLIFHBL@@FF@DH@ABDGFDF@HLDIBAEBCF@@BLFBA@AAFBCDBBFA@FBL@AA@AABBCFEEAFCJUICDI@@@I@AA@FBBDFE@C@AKAEBBBC@EGAEKACA@@B@AGHMHHD@ACGABHBEFVCT@FGADC@DADGDADLPNGBAAAEACF@D@PAH@PELHAEJ@CCKEICAAG@DNDE@CDBF@AEJK@O@DEC@FCICLDNHHDBCANBCE@AGLCDGCHCBCCCA@PECE@K@AJJ@QBNBO@@DODCEBNLFBACBEBFABJCFBOBPKRBB@EAFACGGIC@CGG^DDAFVACFHBBACAJDP@GDM@ABB@GG@@@B@K@EBFFAFB@DGB@FANBH@BQDE@FEBBGJ@FBGBAOAI@DQEJA@KE@ATC@JUG@QADH@@HAB@DDA@JCFFB@AAOCL@MPBAC@@CAQCCAA@@DIUHG@EBUEAIHSERCJK@@GTAD@N@AHRCMEL@HCA@@AICAB@HI@AF@ABLAEW@GBZ@CFNKDCHN@@BDO@@CFL@NDBN@L@AUBHBAEFEEQGAKHQ@@@CAB@KIOCA@CF@P@MA@DLBABARQBA@KCAIDGIDG@JCCBBFBFKED@ABEFBBAHGDBDBBMBDFBDBDDABABD@DPCF\\DB@G@DD@E@G@CV@EBF@O@@CA@BQFDBLAB@BDGHF@@@@BI@DADEBBCDEGCD@A_@EAKACCA@CICDEPKGCEADNABFDCG@DFCBPE@FEDECBAFGBAGCBH@JDAENADAF@CBHACBA@@BCCQCF@FNNBAD@GABKAEBFACGCBFKCGOEBG@DBI@B@GBAJADIOWA@KKLCGFAABAR@TH@BBDAF@DMDA@AHAABREIABAHCED@@EAFHHEBEDSEABNMBAOREEJ@A@ALACIGHFBNPIEDDFDF@@KBHCGBBBOBCAILBFHDMATBDAD@ADFBWHJ@@BACCRC@FJAB@DCFM@IBHCIAF@ED@HAG@@@ECDHBAAHBEKDC@@@BCENC@C@ACDEBDG@A@A@K@BBCNOK@FBCAFIAPFECBBIOEGHDACGBB@@J@ADEECBEG@AACEADD@@JEGACHJ@BDE@JE@IFAQGEBFDDDKUECEEHFD_@CDCG@BJALEB@B@@QEKE@LKBD@ABW@QBNNAL@B@KCADDADA@BD@QBEQ@DQUC@EC@BOGB@AM@DGBAANEIAGFNPEICA@CHEDGDCD@A@LEP@N@CEMFJ@@BIACB@NIBJG@NG^A@EAIMCDHH@BCFEPOJGC@CSNAAAJ@FCGT@FAAOKHXFVFACJHFMAADAA@LA@MHLKTGSZ@HAAJ@AABD@BCBEPDHCJJB@JAABKEBCCEH@D@DBHIAACABAGBBB@EGCACFUDFC@LB@AAFHDBC@G@E@DQD@DKD@CFEBHABANEQ@CCLDAGCAHBA@@@CKC@DAHCGEMJOBGZ@A@CB@AD@ECQ@DAZNN@ACC@@CECDCJAFNJRGBGG@NCBJ@@BI@DBIDCO@B@CCDAAMTGGGCAKDGG@A@ALBA@@FDQ@BA@AB@AVKDJED@@@CDLFC@@C@PCBMCDK@IEHCEAC@CRCOBCAFAGEFUK@@BADGAUFBFIA@H@AI@DDB@BGAAHLVC@KCKBDCBBACHC@A@NI@BEBDIFVF@AGBCIELDIZABAJAFD@AUKS@DJ@@DD@BMHABFBE@IBBDBADAM@GMDBHD@@EAD@BEADGPGGAAFEEABBECKIBAW@BEEQE@DD@FCCAEHJFB@C@E@BMJCB@ALGD@CBC@F@AFJDECAAA@BEO@EBNACDCUH@@K@A@DB@BFECFMAEHDBAEEADFB@EE@CI@ABDBDCAD@A@@B@JCB@FAGJCK@GG@ABFLQ@BHDEHOAB@DBIDSBDBIVA@FD@@ABCAAAA`
-                            switch_branch = 3
+                            async_goto = 3
                             return P.async_await(HtmlRenderer.static_init(), $async$iE)
 
                         case 3:
@@ -4443,7 +4443,7 @@
                             input_name = window.name_input
 
                             h = T.parse_string(input_name)
-
+                            // $.qc -> !test!
                             if (J.Y(J.J(J.J(h, 0)[0], 0), $.qc())) {
                                 $.vr = 6
                                 if (J.aw(h) === 2)
@@ -4456,11 +4456,15 @@
                                         g = d
                                         f = HtmlRenderer.init_out(g)
                                         f.r = 2000
-                                        switch_branch = 1
+                                        async_goto = 1
                                         break
                                     } else {
+                                        // $.nk -> \u0002
                                         e = $.nk()
-                                        if (J.J(h, 0).length === 2 && J.Y(J.J(J.J(h, 0)[1], 0), $.cl())) e = $.cl()
+                                        // $.cl -> !
+                                        if (J.J(h, 0).length === 2 && J.Y(J.J(J.J(h, 0)[1], 0), $.cl())) {
+                                            e = $.cl()
+                                        }
                                         a8 = J.J(h, 1)
                                         a9 = e
                                         a3 = H.set_run_time_type_info([], t.L)
@@ -4472,7 +4476,7 @@
                                         d.d = 1000
                                         c = HtmlRenderer.init_out(d)
                                         c.r = 2000
-                                        switch_branch = 1
+                                        async_goto = 1
                                         break
                                     } if (J.aw(h) === 3) {
                                     a8 = J.J(h, 1)
@@ -4488,36 +4492,36 @@
                                     b.c = 1000
                                     a = HtmlRenderer.init_out(b)
                                     a.r = 2000
-                                    switch_branch = 1
+                                    async_goto = 1
                                     break
                                 }
                             }
-                            switch_branch = 8
+                            async_goto = 8
                             return P.async_await(T.c2(h), $async$iE)
                         case 8:
-                            a0 = b2
+                            a0 = async_result
                             HtmlRenderer.init_out(a0)
                             switch_to = 2
-                            switch_branch = 7
+                            async_goto = 7
                             break
                         case 5:
                             switch_to = 4
-                            b0 = o
+                            b0 = async_result_1
                             a1 = H.unwrap_Exception(b0)
                             H.get_trace_from_exception(b0)
-                            switch_branch = 7
+                            async_goto = 7
                             break
                         case 4:
-                            switch_branch = 2
+                            async_goto = 2
                             break
                         case 7:
                         case 1:
-                            return P.async_return(q, r)
+                            return P.async_return(q, async_completer)
                         case 2:
-                            return P.ai(o, r)
+                            return P.ai(async_result_1, async_completer)
                     }
                 })
-                return P.async_start_sync($async$iE, r)
+                return P.async_start_sync($async$iE, async_completer)
             }
         },
         S = {
